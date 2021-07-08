8e51094bbab7a12f4456cb1887de08981ed3b847
aryanrathod673 <77891627+aryanrathod673@users.noreply.github.com>
2021-07-08 20:56:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var gameState=0;
+var playerCount;
+var database;
+var form,player,game;
+
+function setup(){
+  database = firebase.database();
+ 
+  createCanvas(500,500);
+
+}
+
+function draw(){
+  background("white");
+  
+}
